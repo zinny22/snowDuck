2c898bf1b5418a2a13e30fd4cd6987ad8950d1a3
zinny22 <hziny722@naver.com>
2024-11-22 00:12:09 +0900

    feat : 리액트쿼리 초기 셋팅

--- src/app/(provider)/layout.tsx
+++ src/app/(provider)/layout.tsx
@@ -1,10 +1,21 @@
-// 추후에 사용할 프로바이더를 묶을 레이아웃 모음
-function ProviderLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <div>{children}</div>;
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React, { Suspense, useState } from "react";
+
+function ProvidersLayout({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({ defaultOptions: { queries: { staleTime: 60 * 1000 } } })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Suspense fallback={<div id="container"></div>}>{children}</Suspense>
+    </QueryClientProvider>
+  );
 }
 
-export default ProviderLayout;
+export default ProvidersLayout;
---
+++ src/app/(provider)/page.tsx
@@ -0,0 +1,10 @@
+function HomePage() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1>랜딩 페이지</h1>
+      <button>??</button>
+    </div>
+  );
+}
+
+export default HomePage;
